f060dbb8585c5bcca781679d40220adbd55fd4fe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-05 20:32:31 +0000

    Deploy foodpicker.club to github.com/christopherkade/fastfood.git:gh-pages

---
+++ precache-manifest.9f25af606e0154771c5e046f0db9303f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe1b35e0cdc8d50e231e518c4a58913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17b1c73cb5e609b841e3",
+    "url": "/static/css/main.0f4e07a3.chunk.css"
+  },
+  {
+    "revision": "d3cc541a06d86548b724",
+    "url": "/static/js/2.823480cd.chunk.js"
+  },
+  {
+    "revision": "17b1c73cb5e609b841e3",
+    "url": "/static/js/main.a9ccf356.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
